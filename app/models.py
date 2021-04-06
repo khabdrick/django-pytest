dc4f60fce63421af60b49dc5dc24146d758c6855
Muhammed Ali <muhamzyali@gmail.com>
2021-04-06 23:41:49 +0100

    added modes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(default=timezone.now)
+
