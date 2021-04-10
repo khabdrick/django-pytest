243d64819450c874bb690f1930f0eec2ecf094a7
Muhammed Ali <muhamzyali@gmail.com>
2021-04-10 19:26:03 +0100

    test for url

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -4,5 +4,5 @@ from django.urls import reverse, resolve
 class TestUrls:
     
     def test_post_content_url(self):
-        path = reverse('content', kwargs={'pk':1}) 
+        path = reverse("content", kwargs={'pk':1}) 
         assert resolve(path).view_name == "content"
