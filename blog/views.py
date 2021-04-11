d3c4727826bb584c13c0073ea9095550d4d3c416
Muhammed Ali <muhamzyali@gmail.com>
2021-04-11 12:38:08 +0100

    updated views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def post_content(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "post_content.html", {'post':post})
