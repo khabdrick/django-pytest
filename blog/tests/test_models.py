72a94d698dda2366fed5fbda2acf84bc6fc4e5e9
Muhammed Ali <muhamzyali@gmail.com>
2021-04-12 13:54:58 +0100

    added more tests

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,5 +1,7 @@
 import pytest
 from blog.models import Post
+from mixer.backend.django import mixer
+
 @pytest.mark.django_db
 class TestModels:
 
@@ -15,3 +17,7 @@ class TestModels:
         assert post.title == "Simple article"
         assert post.content == "This is my content"
         assert post.date_posted == "2021-04-12 12:12:11.343944+00:00"
+
+    def test_model_display_title(self):
+        post = mixer.blend(Post, title = "Testing")
+        assert  str(post) == "Testing" 
